f0d99425fb5516168315a2ed7c80348ad0de7986
Dmitry <darksky484@gmail.com>
2026-01-17 00:13:32 +0200

    Update SVG icon for 'Hide Shots' component

--- shot.js
+++ shot.js
@@ -72,9 +72,9 @@
             component: 'hide_shots',
             name: 'Hide Shots',
             icon:
-                '<svg viewBox="0 0 24 24" fill="currentColor">' +
-                '<path d="M3 4h18v2H3zm0 7h18v2H3zm0 7h18v2H3z"/>' +
-                '</svg>'
+            '<svg viewBox="0 0 512 512" fill="currentColor">' +
+            '<path d="M253.266 512a19.166 19.166 0 0 1-19.168-19.168V330.607l-135.071-.049a19.164 19.164 0 0 1-16.832-28.32L241.06 10.013a19.167 19.167 0 0 1 36.005 9.154v162.534h135.902a19.167 19.167 0 0 1 16.815 28.363L270.078 502.03a19.173 19.173 0 0 1-16.812 9.97z"/>' +
+            '</svg>'
         });
 
         Lampa.SettingsApi.addParam({
@@ -92,6 +92,8 @@
                 applyState(value);
             }
         });
+
+        Lampa.SettingsApi.update();
 
         applyState(Lampa.Storage.get(STORAGE_KEY, true));
     });
